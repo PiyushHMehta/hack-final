a669ac104b1dfa28c9c08380a400ba14990a72f7
KrrishNichanii <krrishnichanii@gmail.com>
2024-04-27 15:20:45 +0530

    Added Interests.jsx

--- client/src/components/Interests.jsx
+++ client/src/components/Interests.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React , {useState} from 'react'
 
-export default function () {
+export default function ({setToggle , setInterests , interests}) {
+    //const [preferences , setPreferences] = useState([]) ; 
     const academicInterests = [
         "Artificial Intelligence (AI)",
         "Machine Learning (ML)",
@@ -17,14 +18,22 @@ export default function () {
     //   console.log(academicInterests);
       
     return (
-        <div className='grid grid-cols-5 text-black'>
+        <div className="">
+        <ul className='grid grid-cols-5 text-black gap-4 rounded-sm'>
             {
-                academicInterests.map((interest, index) => {
-                    <div className='p-4 flex' >
+                academicInterests.map((interest, index) => (
+                    <li 
+                    value={interest}
+                    onClick={(e) => setInterests([...interests , interest])}
+                    key={interest} className={`p-4 flex  text-white gap-3 cursor-pointer ${interests.includes(interest)? "bg-green-500":"bg-black"}`} >
                         {interest}
-                    </div>
-                })
+                        {/* {console.log('Interest ',interest)} */}
+                    </li>
+                ))
             }
+        </ul>
+
+        <button onClick={() => setToggle(true)}>Previous</button>
         </div>
     )
 }
